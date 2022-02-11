5c29c97db4de0fa2c104113bd3dd32cc2abd58cf
fathela-domm <dommmmbuga59@gmail.com>
2022-02-11 19:58:54 +0300

    rectified some grammatical error

--- src/Home/project-wrap.component.js
+++ src/Home/project-wrap.component.js
@@ -8,13 +8,12 @@ export function ProjectWrapComponent(props) {
                 <div className="row">
                     <div className="col-lg-6">
                         <div className="section-heading">
-                            <h1 className="text-white">We are trying to deliver 100% quality services and success their business
-                                to achieve ultimate goal.</h1>
+                            <h1 className="text-white">We are trying to deliver 100% quality services for our clients to achieve their ultimate goal.</h1>
                         </div>
                     </div>
                     <div className="col-lg-6">
-                        <p className="lead text-white">We have been providing exceptional real estates services since time immemorial. We are committed to provide quality
-                            services just for you. </p>
+                        <p className="lead text-white">We have been providing exceptional real estates services to our customers since time immemorial and are committed to surpass your expectation
+                            in service delivery. </p>
                     </div>
                 </div>
             </div>
