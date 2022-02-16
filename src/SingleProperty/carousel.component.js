da42c5e3dba4217000c411b651026380548fd1bc
fathela-domm <dommmmbuga59@gmail.com>
2022-02-16 07:43:58 +0300

    fixed a bug in admin section

--- src/SingleProperty/carousel.component.js
+++ src/SingleProperty/carousel.component.js
@@ -5,7 +5,7 @@ export function CarouselComponent({ property }) {
 
     return (
         <div className='col-sm-12'>
-            <Carousel style={{ width: "100%", height: "70vh" }}>
+            <Carousel duration={3000} style={{ width: "100%", height: "70vh" }}>
                 {
                     property && property.images.map(img => {
                         return <img style={{ width: "100%", height: "80vh" }} src={img} alt="carousel-image" />
